fc727aa39e2fa3f5349c760157944df12798afcb
HaNaK0 <hampus@hampushuledal.com>
2025-03-11 22:16:32 +0100

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{fs::File, io::BufReader, io::BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("setup.csv").unwrap();
+
+    let lines = BufReader::new(file).lines();
+    let lines: Vec<String>= lines.map(|l| l.expect("failed reading line")).collect();
+
 }
+
